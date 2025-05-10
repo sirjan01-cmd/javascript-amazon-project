b3cfc8c6c7f2d255310dd89c1dbbbbc3ddf90501
sirach-cmd <sirachdeguzman7@gmail.com>
2025-05-10 04:08:13 -0500

    Learn error handling

--- data/products.js
+++ data/products.js
@@ -99,8 +99,12 @@ export function loadProductsFetch() {
       }
       return new Product(productDetails);
     });
+
     console.log('load products');
-  });
+  }).catch((error) => {
+      console.log('Unexpected error. Please try again later.');
+    });
+
   return promise;
 }
 /*
@@ -122,6 +126,10 @@ export function loadProducts(fun) {
     console.log('load products');
 
     fun();
+  });
+
+  xhr.addEventListener('error', (error) => {
+    console.log('unexpected error. Please try again later.');
   });
 
   xhr.open('GET', 'https://supersimplebackend.dev/products');
