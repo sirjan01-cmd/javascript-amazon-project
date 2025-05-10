ba5302e62d0b2cba6fd82f7409f8170fc9818253
sirach-cmd <sirachdeguzman7@gmail.com>
2025-05-10 03:33:04 -0500

    Convert from promises to async await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -5,6 +5,22 @@ import {loadCart} from '../data/cart.js';
 // import '../data/cart-class.js';
 // import '../data/back-end-practice.js';
 
+async function loadPage() {
+
+await loadProductsFetch();
+
+ const value = await new Promise((resolve) => {
+    loadCart(() => {
+      resolve('value3');
+    });
+  });
+
+  renderOrderSummary();
+  renderPaymentSummary();
+}
+loadPage();
+
+/*
 Promise.all([
 loadProductsFetch(), 
   new Promise((resolve) => {
@@ -18,6 +34,7 @@ loadProductsFetch(),
   renderOrderSummary();
   renderPaymentSummary();
 });
+*/
 
 /*
 new Promise((resolve) => {
